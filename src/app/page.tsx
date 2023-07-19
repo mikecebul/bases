d904e0a7570f87e52ad2d9097298aceddcc1c1e3
mikecebul <mikecebul@gmail.com>
2023-07-19 17:59:09 -0400

    Added Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "@/components/hero";
+
 export default function Home() {
-  return <main className="">Home</main>;
+  return (
+    <main className="">
+      <Hero />
+    </main>
+  );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import { MobileNav } from "./mobile-nav";
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-40 flex w-full border-b border-secondary bg-background/10 backdrop-blur-sm">
+    <header className="sticky top-0 z-40 flex w-full bg-background/50 backdrop-blur-sm">
       <div className="px-4 lg:container flex items-center h-14 lg:h-20 w-full">
         <Icons.logo className="w-40 md:w-44 lg:w-64" />
         <MainNav />
