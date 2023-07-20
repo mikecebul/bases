58442999e9fe6e8bc96d72c991c12aef8d4e51ee
mikecebul <mikecebul@gmail.com>
2023-07-20 17:37:43 -0400

    Add Logo Link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Icons } from "./icons";
 
 export default function Footer() {
@@ -5,12 +6,9 @@ export default function Footer() {
     <footer className="rounded-t-md shadow bg-background/50">
       <div className="w-full mx-auto p-4 md:px-8 md:py-8 container 2xl:px-0">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a
-            href="https://basesmi.org/"
-            className="flex items-center mb-4 sm:mb-0"
-          >
+          <Link href="/">
             <Icons.logo className="w-40 md:w-44 lg:w-64" />
-          </a>
+          </Link>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
               <a href="#about" className="mr-4 hover:underline md:mr-6 ">
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,9 @@ export function Header() {
   return (
     <header className="sticky top-0 z-40 flex w-full bg-background/50 backdrop-blur-sm">
       <div className="flex items-center w-full px-4 md:px-8 2xl:px-0 2xl:container h-14 lg:h-20">
+        <Link href="/">
         <Icons.logo className="w-40 md:w-44 lg:w-64" />
+        </Link>
         <MainNavLandingPage />
         <MobileNavLandingPage />
         <Link href="tel:2315471144" legacyBehavior passHref>
