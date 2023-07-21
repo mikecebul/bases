cad74406925bae60dfa92d747f2281f25920e6a2
mikecebul <mikecebul@gmail.com>
2023-07-20 21:12:35 -0400

    Move svg to services

--- src/components/about-section.tsx
+++ src/components/about-section.tsx
@@ -7,37 +7,6 @@ export default function AboutSection() {
       id="about"
       className="relative px-4 py-24 overflow-hidden isolate md:px-8 sm:py-32 2xl:px-0 xl:overflow-visible 2xl:container"
     >
-      <div className="absolute inset-0 overflow-hidden -z-10">
-        <svg
-          className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
-          aria-hidden="true"
-        >
-          <defs>
-            <pattern
-              id="e813992c-7d03-4cc4-a2bd-151760b470a0"
-              width={200}
-              height={200}
-              x="50%"
-              y={-1}
-              patternUnits="userSpaceOnUse"
-            >
-              <path d="M100 200V.5M.5 .5H200" fill="none" />
-            </pattern>
-          </defs>
-          <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
-            <path
-              d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
-              strokeWidth={0}
-            />
-          </svg>
-          <rect
-            width="100%"
-            height="100%"
-            strokeWidth={0}
-            fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)"
-          />
-        </svg>
-      </div>
       <div className="grid grid-cols-1 gap-x-8 gap-y-16 xl:container xl:grid-cols-2 xl:items-start xl:gap-y-10 xl:gap-x-10 xl:px-0">
         <div className="xl:col-span-2 xl:col-start-2 xl:row-start-1 xl:grid xl:w-full xl:gap-x-8 xl:px-0">
           <div className="">
--- src/components/services.tsx
+++ src/components/services.tsx
@@ -3,7 +3,38 @@ import { siteConfig } from "@/config/site";
 
 export default function Services() {
   return (
-    <div id="services" className="py-24 sm:py-3">
+    <div id="services" className="py-24 sm:py-3 relative">
+      <div className="absolute inset-0 overflow-hidden -z-10">
+        <svg
+          className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="e813992c-7d03-4cc4-a2bd-151760b470a0"
+              width={200}
+              height={200}
+              x="50%"
+              y={-1}
+              patternUnits="userSpaceOnUse"
+            >
+              <path d="M100 200V.5M.5 .5H200" fill="none" />
+            </pattern>
+          </defs>
+          <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
+            <path
+              d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
+              strokeWidth={0}
+            />
+          </svg>
+          <rect
+            width="100%"
+            height="100%"
+            strokeWidth={0}
+            fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)"
+          />
+        </svg>
+      </div>
       <div className="2xl:container px-4 md:px-8 2xl:px-0">
         <div className="max-w-2xl">
           <p className="text-base font-semibold leading-7 text-brand">
