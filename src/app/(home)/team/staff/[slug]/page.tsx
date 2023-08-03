68e267eec7a318d4cd23d2f5727c2ec085ff4432
Mike Cebulski <mikecebul@gmail.com>
2023-08-02 23:44:39 -0400

    revalidate team every 60 seconds

--- src/app/(home)/team/staff/[slug]/page.tsx
+++ src/app/(home)/team/staff/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import prisma from "@/lib/prisma";
 import StaffBio from "@/components/staff-bio";
 
+export const revalidate = 60
+
 export async function generateStaticParams() {
   const StaffMembers = await prisma.staffMember.findMany();
   return StaffMembers.map((person) => ({
