7fe2c9b3f18b6b2ba126a34a18c5ec565be3ae6a
Mike Cebulski <mikecebul@gmail.com>
2023-08-02 23:28:46 -0400

    Update route.tsx

--- src/app/api/revalidate/route.tsx
+++ src/app/api/revalidate/route.tsx
@@ -3,7 +3,9 @@ import { revalidatePath } from "next/cache";
 
 export async function GET(request: NextRequest) {
   const path = request.nextUrl.searchParams.get("path");
-  revalidatePath(path);
+  if (!!path) {
+    revalidatePath(path!)
+  }
   revalidatePath('/team/staff/[slug]')
   console.log("Revalidate Path: ", path);
   return NextResponse.json({ revalidated: true, now: Date.now() });
