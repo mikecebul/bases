3231a62a09d747b647dcb5e780737e1c585804d7
mikecebul <mikecebul@gmail.com>
2023-08-07 18:21:55 -0400

    Fix: sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -14,12 +14,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   const boardMembers = siteConfig.team.boardMembers
   const boardMemberPages = boardMembers.map((person) => ({
-    url: `${URL}/team/staff/${person.slug}`,
+    url: `${URL}/team/board-members/${person.slug}`,
     lastModified: newDate
   }));
 
   const routes = siteConfig.NavLinks.map((route) => ({
-    url: `${URL}${route}`,
+    url: `${URL}${route.href}`,
     lastModified: newDate
   }))
 
