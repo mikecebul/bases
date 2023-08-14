d8af49d69cfe0574fa6b3d08bddbeaa8390134f6
mikecebul <mikecebul@gmail.com>
2023-08-14 18:24:12 -0400

    Robots: disallow /ahse

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/admin",
+      disallow: ["/admin", "/ahse"],
     },
     sitemap: `${process.env.NEXT_PUBLIC_DOMAIN_URL}/sitemap.xml`,
   };
