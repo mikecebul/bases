e8e56a4af748ec41d8cbf7a4d4d2715e016a0e1b
mikecebul <mikecebul@gmail.com>
2023-08-28 01:23:39 -0400

    Testing new staff member with revalidate

--- src/components/admin/staffCreateForm.tsx
+++ src/components/admin/staffCreateForm.tsx
@@ -19,6 +19,7 @@ import { useRouter } from "next/navigation";
 import { Input } from "../ui/input";
 import FirstStaffProfilePicture from "./first-staff-profile-picture";
 import { CreateStaffMemberAction } from "@/actions/create-staff-member-action";
+import { generateSlug, revalidate } from "@/lib/utils";
 
 const staffFormSchema = z.object({
   name: z.string(),
@@ -111,11 +112,13 @@ export default function StaffCreateForm() {
     } else {
       toast({ description: "Profile was updated successfully." });
       router.push("/admin/staff");
+      await revalidate("/team")
+      await revalidate(`/team/staff/${generateSlug(newStaffMemberData.name)}`)
     }
   }
 
   return (
-    <div className="max-w-7xl p-8">
+    <div className="p-8 max-w-7xl">
       <FirstStaffProfilePicture imageUrl={imageUrl} setImageUrl={setImageUrl} />
       <Form {...form}>
         <form
