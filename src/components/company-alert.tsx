503253b970d6afd0633cb9a8cd09546d7298e6ee
mikecebul <mikecebul@gmail.com>
2023-12-12 15:17:07 -0500

    fix: ui updates to company alert

--- src/components/company-alert.tsx
+++ src/components/company-alert.tsx
@@ -10,6 +10,8 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "./ui/alert-dialog";
+import { buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
 
 export function CompanyAlert() {
   const [isOpen, setIsOpen] = useState(true);
@@ -20,18 +22,27 @@ export function CompanyAlert() {
   return (
     mounted && (
       <AlertDialog open={isOpen} onOpenChange={() => setIsOpen(!isOpen)}>
-        <AlertDialogContent className="bg-red-100">
+        <AlertDialogContent className="bg-red-300">
           <AlertDialogHeader>
-            <AlertDialogTitle>Holiday Closure Dates</AlertDialogTitle>
-            <div className="pl-4">
+            <AlertDialogTitle className="">
+              Holiday Closure Dates
+            </AlertDialogTitle>
+            <div className="pt-4 pl-4">
+              <p className="font-semibold">Christmas</p>
               <p>Friday, December 22, 2023</p>
               <p>Monday, December 25, 2023</p>
+              <p className="pt-4 font-semibold">New Years</p>
               <p>Friday, December 29, 2023</p>
               <p>Monday, January 1, 2024</p>
             </div>
           </AlertDialogHeader>
           <AlertDialogFooter>
-            <AlertDialogCancel className="bg-red-300 hover:bg-red-400 ring-red-400 ring-offset-red-400">
+            <AlertDialogCancel
+              className={cn(
+                buttonVariants({ variant: "brand" }),
+                "hover:text-primary-foreground"
+              )}
+            >
               Close
             </AlertDialogCancel>
           </AlertDialogFooter>
