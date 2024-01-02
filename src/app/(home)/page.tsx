d1548022f6fa74344782049f168d28f3baa38fce
mikecebul <mikecebul@gmail.com>
2024-01-02 16:27:49 -0500

    Chore: remove company alert from homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,7 +3,6 @@ import FrontPageServices from "@/components/services-front-page";
 import Hero from "@/components/hero";
 import Donate from "@/components/donate";
 import prisma from "@/lib/prisma";
-import { CompanyAlert } from "@/components/company-alert";
 
 async function getFrontPageServices() {
   const services = await prisma.service.findMany({
@@ -22,7 +21,6 @@ export default async function Home() {
   const services = await getFrontPageServices();
   return (
     <>
-      <CompanyAlert />
       <Hero />
       <FrontPageServices services={services} />
       <Carf />
