5f84e6edaad7fce22edc797417da562810e71060
mikecebul <mikecebul@gmail.com>
2024-06-03 02:04:38 -0400

    Fix: tailwind mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,7 @@
-export const plugins = {
-  tailwindcss: {},
-  autoprefixer: {},
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
