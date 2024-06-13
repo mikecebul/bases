babfc4c35dfb9dc8903380976c273a0eb905117d
mikecebul <mikecebul@gmail.com>
2024-06-13 13:21:44 -0400

    Fix: removing prisma calls

--- src/app/(home)/_team/staff/[slug]/page.tsx
+++ src/app/(home)/_team/staff/[slug]/page.tsx
@@ -1,57 +1,57 @@
-import prisma from "@/lib/prisma";
-import StaffBio from "@/components/staff-bio";
-import { Metadata } from "next";
+// import prisma from "@/lib/prisma";
+// import StaffBio from "@/components/staff-bio";
+// import { Metadata } from "next";
 
-interface Params {
-  slug: string;
-}
+// interface Params {
+//   slug: string;
+// }
 
-async function getStaffMember(slug: string) {
-  const staffMember = await prisma.staffMember.findFirst({
-    where: {
-      slug: slug,
-    },
-  });
-  return staffMember;
-}
+// async function getStaffMember(slug: string) {
+//   const staffMember = await prisma.staffMember.findFirst({
+//     where: {
+//       slug: slug,
+//     },
+//   });
+//   return staffMember;
+// }
 
-export async function generateMetadata({
-  params,
-}: {
-  params: Params;
-}): Promise<Metadata> {
-  const { slug } = params;
-  const staffMember = await prisma.staffMember.findFirst({
-    where: {
-      slug: slug,
-    },
-  });
-  return {
-    title: `Staff Member - ${staffMember?.name}` || "Staff profile page",
-    description:
-      `Learn about the history and experience of our staff member ${staffMember?.name}.` ||
-      "Learn about our staff members education, background, and expertise.",
-  };
-}
+// export async function generateMetadata({
+//   params,
+// }: {
+//   params: Params;
+// }): Promise<Metadata> {
+//   const { slug } = params;
+//   const staffMember = await prisma.staffMember.findFirst({
+//     where: {
+//       slug: slug,
+//     },
+//   });
+//   return {
+//     title: `Staff Member - ${staffMember?.name}` || "Staff profile page",
+//     description:
+//       `Learn about the history and experience of our staff member ${staffMember?.name}.` ||
+//       "Learn about our staff members education, background, and expertise.",
+//   };
+// }
 
-export async function generateStaticParams() {
-  const staffMembers = await prisma.staffMember.findMany({
-    where: {
-      status: "PUBLISHED",
-    },
-  });
-  return staffMembers.map((person) => ({
-    slug: person.slug,
-  }));
-}
+// export async function generateStaticParams() {
+//   const staffMembers = await prisma.staffMember.findMany({
+//     where: {
+//       status: "PUBLISHED",
+//     },
+//   });
+//   return staffMembers.map((person) => ({
+//     slug: person.slug,
+//   }));
+// }
 
-export default async function Page({ params }: { params: Params }) {
-  const { slug } = params;
-  const staffMember = await getStaffMember(slug);
+// export default async function Page({ params }: { params: Params }) {
+//   const { slug } = params;
+//   const staffMember = await getStaffMember(slug);
 
-  if (!staffMember) {
-    return <div className="">Staff member not found</div>;
-  }
+//   if (!staffMember) {
+//     return <div className="">Staff member not found</div>;
+//   }
 
-  return <StaffBio staffMember={staffMember} />;
-}
+//   return <StaffBio staffMember={staffMember} />;
+// }
--- src/lib/uploadthing.ts
+++
@@ -1,5 +0,0 @@
-import { OurFileRouter } from "@/app/api/_uploadthing/core";
-import { generateComponents } from "@uploadthing/react";
-
-export const { UploadButton, UploadDropzone } =
-  generateComponents<OurFileRouter>();
