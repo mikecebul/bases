d70c9bee56bb1813225644bcbc449cfb521aff67
mikecebul <mikecebul@gmail.com>
2024-06-13 13:35:53 -0400

    Fix: Remove Toaster

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -5,7 +5,6 @@ import { Header } from "@/components/header";
 import { cn } from "@/lib/utils";
 import Footer from "@/components/footer";
 import { Analytics } from "@vercel/analytics/react";
-import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,7 +27,6 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
-        <Toaster />
         <Analytics />
       </body>
     </html>
