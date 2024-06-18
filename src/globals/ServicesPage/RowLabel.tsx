c7202c35fef6d32fa030705327fa2c86c264071d
mikecebul <mikecebul@gmail.com>
2024-06-18 10:59:49 -0400

    Chore: Cleanup RowLabel further

--- src/globals/ServicesPage/RowLabel.tsx
+++ src/globals/ServicesPage/RowLabel.tsx
@@ -3,37 +3,31 @@
 import { lucideIcons } from "@/components/icons";
 import { useRowLabel } from "@payloadcms/ui/forms/RowLabel/Context";
 import { useMemo } from "react";
-import { Service } from "@/payload-types";
+import type { Service } from "@/payload-types";
 import useSWR from "swr";
+import { fetcher } from "@/lib/utils";
 
 type RowData = {
   id: string;
   service: string;
 };
 
-const fetcher = (url: string): Promise<Service> =>
-  fetch(url).then((res) => {
-    if (!res.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return res.json();
-  });
-
 function RowLabel() {
-  const { data, rowNumber } = useRowLabel<RowData>();
+  const {
+    data: { service: serviceId },
+    rowNumber,
+  } = useRowLabel<RowData>();
 
-  const { data: relationshipData } = useSWR<Service>(
-    data.service
-      ? `${process.env.NEXT_PUBLIC_DOMAIN_URL}/api/services/${data.service}`
+  const { data: service } = useSWR<Service>(
+    serviceId
+      ? `${process.env.NEXT_PUBLIC_DOMAIN_URL}/api/services/${serviceId}`
       : null,
     fetcher
   );
 
   const Icon = useMemo(
-    () =>
-      lucideIcons.find((icon) => icon.value === relationshipData?.icon)
-        ?.component,
-    [relationshipData]
+    () => lucideIcons.find((icon) => icon.value === service?.icon)?.component,
+    [service]
   );
 
   if (!Icon) return <span>{`${rowNumber} - Service`}</span>;
@@ -42,9 +36,7 @@ function RowLabel() {
     <div className="flex flex-row items-center">
       <span className="text-lg font-semibold mr-8">{rowNumber}</span>
       <Icon className="w-8 h-6" />
-      <span className="text-lg font-semibold ml-4">
-        {relationshipData?.title}
-      </span>
+      <span className="text-lg font-semibold ml-4">{service?.title}</span>
     </div>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -51,3 +51,11 @@ export async function revalidate(path: string) {
     console.error(err);
   }
 }
+
+export const fetcher = (url: string) =>
+  fetch(url).then((res) => {
+    if (!res.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return res.json();
+  });
