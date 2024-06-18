be17384f9ec29421d4504bb6b3cb003fe8c8b188
mikecebul <mikecebul@gmail.com>
2024-06-18 11:03:13 -0400

    Chore: Cleanup RowLabel even further

--- src/globals/ServicesPage/RowLabel.tsx
+++ src/globals/ServicesPage/RowLabel.tsx
@@ -30,13 +30,13 @@ function RowLabel() {
     [service]
   );
 
-  if (!Icon) return <span>{`${rowNumber} - Service`}</span>;
+  if (!Icon || !service) return <span>{`${rowNumber} - Service`}</span>;
 
   return (
     <div className="flex flex-row items-center">
       <span className="text-lg font-semibold mr-8">{rowNumber}</span>
       <Icon className="w-8 h-6" />
-      <span className="text-lg font-semibold ml-4">{service?.title}</span>
+      <span className="text-lg font-semibold ml-4">{service.title}</span>
     </div>
   );
 }
