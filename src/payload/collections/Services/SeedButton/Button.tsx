18c03408019f4c356b9a2f4c8e28f7e5b30c0f3d
mikecebul <mikecebul@gmail.com>
2024-07-01 18:22:47 -0400

    Chore: moving Seed Services from endpoint to server action

--- src/payload/collections/Services/SeedButton/Button.tsx
+++ src/payload/collections/Services/SeedButton/Button.tsx
@@ -1,51 +1,30 @@
 "use client";
 
-import React, { Fragment, useCallback, useState } from "react";
+import React, { Fragment, useActionState } from "react";
 import { Button as PayloadButton } from "@payloadcms/ui";
-import { useRouter } from "next/navigation";
 import { createServices } from "./action";
 
-export const Button: React.FC = () => {
-  const [loading, setLoading] = useState(false);
-  const [seeded, setSeeded] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-
-  const router = useRouter();
-
-  const handleClick = async () => {
-    if (loading || seeded) return;
-
-    setLoading(true);
-    setError("");
-
-    try {
-      await createServices();
-      setSeeded(true);
-    } catch (err) {
-      if (err instanceof Error) {
-        setError(err.message);
-      }
-    } finally {
-      setLoading(false);
-      router.refresh();
-    }
-  };
-
-  let message = "";
-  if (loading) message = " (seeding...)";
-  if (seeded) message = " (done!)";
-  if (error) message = ` (error: ${error})`;
+export const Button = () => {
+  const [state, action, isPending] = useActionState(createServices, null);
 
   return (
     <Fragment>
       <PayloadButton
         buttonStyle="secondary"
         className="w-52"
-        onClick={handleClick}
+        onClick={() => {
+          action();
+        }}
+        disabled={isPending}
       >
-        Seed Services
+        {isPending ? "Seeding..." : "Seed Services"}
       </PayloadButton>
-      <p>{message}</p>
+      {!!state?.error ? (
+        <p className="text-red-500">Error: {state.error}</p>
+      ) : null}
+      {!!state?.success ? (
+        <p className="text-green-500">Success: {state.success}</p>
+      ) : null}
     </Fragment>
   );
 };
--- src/payload/collections/Services/SeedButton/action.ts
+++ src/payload/collections/Services/SeedButton/action.ts
@@ -3,20 +3,31 @@
 import { oldSiteConfig } from "@/app/config/site";
 import payloadConfig from "@payload-config";
 import { getPayloadHMR } from "@payloadcms/next/utilities";
+import { revalidatePath } from "next/cache";
 
-export async function createServices() {
+export async function createServices(req: any) {
+  console.log("Req:", req);
   const payload = await getPayloadHMR({
     config: payloadConfig,
   });
-  oldSiteConfig.Services.forEach(
-    async (service) =>
-      await payload.create({
-        collection: "services",
-        data: {
-          title: service.name,
-          desc: service.description,
-          icon: service.icon,
-        },
-      })
-  );
+
+  try {
+    oldSiteConfig.Services.forEach(
+      async (service) =>
+        await payload.create({
+          collection: "services",
+          data: {
+            title: service.name,
+            desc: service.description,
+            icon: service.icon,
+          },
+        })
+    );
+    revalidatePath("/(payload)/admin/collections/services", "page");
+    return { success: true };
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : "Unknown error";
+    payload.logger.error(message);
+    return { error: message };
+  }
 }
