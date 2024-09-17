3c353e2241e5a9970c40e6ea4c7b9ea0b9f73446
mikecebul <mikecebul@gmail.com>
2024-09-17 13:08:05 -0400

    Test: manually enter media prefix

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -205,7 +205,7 @@ export default buildConfig({
         landscapes: {
           disableLocalStorage: true,
           generateFileURL: (args: any) => {
-            return `https://${process.env.NEXT_PUBLIC_S3_HOSTNAME}/${args.prefix}/${args.filename}`
+            return `https://${process.env.NEXT_PUBLIC_S3_HOSTNAME}/landscapes/${args.filename}`
           },
           prefix: 'landscapes',
         },
