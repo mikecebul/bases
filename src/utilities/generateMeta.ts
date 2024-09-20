87c092ce95d687091633cf4619787e62acae490b
mikecebul <mikecebul@gmail.com>
2024-09-20 16:18:27 -0400

    Fix: use the correct image url when using S3

--- src/utilities/generateMeta.ts
+++ src/utilities/generateMeta.ts
@@ -10,8 +10,11 @@ export const generateMeta = async (args: { doc: Page | Team }): Promise<Metadata
   const ogImage =
     typeof doc?.meta?.metadata?.image === 'object' &&
     doc.meta.metadata.image !== null &&
-    'url' in doc.meta.metadata.image &&
-    `${process.env.NEXT_PUBLIC_SERVER_URL}${doc.meta.metadata.image.url}`
+    'url' in doc.meta.metadata.image
+      ? process.env.S3_ENABLED === 'true'
+        ? doc.meta.metadata.image.url
+        : `${process.env.NEXT_PUBLIC_SERVER_URL}${doc.meta.metadata.image.url}`
+      : undefined
 
   const title = doc?.meta?.metadata?.title ? doc.meta.metadata.title + ' | BASES' : 'BASES'
 
