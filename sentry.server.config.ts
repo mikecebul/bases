530f26811a5248968db946315711464342416841
mikecebul <mikecebul@gmail.com>
2025-04-25 14:56:32 -0400

    fix build errors and use env for sentry url

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: 'https://ca418f305cde4415aa5ef2d901a9f2cb@monitor.mikecebul.dev/2',
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
