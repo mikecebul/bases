a4753bdf6b8dc53d17e10368757a6656722e1a94
mikecebul <mikecebul@gmail.com>
2025-05-06 14:57:06 -0400

    Disable GoogleMaps API in Development

--- src/globals/Footer/GoogleMap.tsx
+++ src/globals/Footer/GoogleMap.tsx
@@ -2,34 +2,39 @@
 
 import { CompanyInfo } from '@/payload-types'
 import { APIProvider, InfoWindow, Map } from '@vis.gl/react-google-maps'
-import { LandPlot } from 'lucide-react'
-
 
 export const GoogleMap = ({ contact }: { contact: CompanyInfo['contact'] }) => {
-  const { name, physicalAddress: { coordinates, street, cityStateZip } } = contact
+  const {
+    name,
+    physicalAddress: { coordinates, street, cityStateZip },
+  } = contact
   const defaultPosition = { lat: 45.30352, lng: -85.25986 }
 
-  const position = coordinates
-    ? { lat: coordinates[0], lng: coordinates[1] }
-    : defaultPosition
+  const position = coordinates ? { lat: coordinates[0], lng: coordinates[1] } : defaultPosition
+
+  if (process.env.NODE_ENV !== 'production') {
+    return (
+      <div className="h-[350px] bg-gray-100 flex items-center justify-center flex-col gap-2">
+        <div className="text-sm text-gray-500">
+          <p>{name}</p>
+          <p>{street}</p>
+          <p>{cityStateZip}</p>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div className="h-[350px]">
       <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
-        <Map
-          defaultZoom={15}
-          defaultCenter={position}
-          disableDefaultUI
-          fullscreenControl
-
-        >
+        <Map defaultZoom={15} defaultCenter={position} disableDefaultUI fullscreenControl>
           <InfoWindow
             position={position}
             shouldFocus={false}
             headerContent={
-              <div className="flex gap-4 items-center">
+              <div className="flex items-center gap-4">
                 <div className="flex flex-col gap-4">
-                  <h3 className="text-base font-bold text-balance leading-5">{name}</h3>
+                  <h3 className="text-base font-bold leading-5 text-balance">{name}</h3>
                 </div>
               </div>
             }
