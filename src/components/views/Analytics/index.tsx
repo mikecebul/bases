16d8bd3c9cbb51daf4601eefe7b18119bc4450ac
mikecebul <mikecebul@gmail.com>
2025-05-09 12:09:57 -0400

    fix: use SetStepNav to set breadcrumbs in root view

--- src/components/views/Analytics/index.tsx
+++ src/components/views/Analytics/index.tsx
@@ -1,14 +1,19 @@
-import type { AdminViewServerProps } from "payload";
+import type { AdminViewServerProps } from 'payload'
 
-import { DefaultTemplate } from "@payloadcms/next/templates";
-import { Gutter } from "@payloadcms/ui";
-import React from "react";
+import { DefaultTemplate } from '@payloadcms/next/templates'
+import { Gutter, SetStepNav } from '@payloadcms/ui'
 
 const AnalyticsDefaultRootView: React.FC<AdminViewServerProps> = ({
   initPageResult,
   params,
   searchParams,
 }) => {
+  const navItem = [
+    {
+      label: 'Analytics',
+      url: '/analytics',
+    },
+  ]
   return (
     <DefaultTemplate
       i18n={initPageResult.req.i18n}
@@ -20,19 +25,20 @@ const AnalyticsDefaultRootView: React.FC<AdminViewServerProps> = ({
       user={initPageResult.req.user || undefined}
       visibleEntities={initPageResult.visibleEntities}
     >
+      <SetStepNav nav={navItem} />
       <Gutter>
-      <iframe
-        src="https://analytics.mikecebul.dev/share/OiX0b1uO00C1Cgf3/basesmi.org"
-        width="100%"
-        height="100%"
-        style={{ border: 'none', overflow: 'hidden', minHeight: '85dvh' }}
-        title="Analytics"
-        allowFullScreen
-        loading="lazy"
-      />
+        <iframe
+          src="https://analytics.mikecebul.dev/share/OiX0b1uO00C1Cgf3/basesmi.org"
+          width="100%"
+          height="100%"
+          style={{ border: 'none', overflow: 'hidden', minHeight: '85dvh' }}
+          title="Analytics"
+          allowFullScreen
+          loading="lazy"
+        />
       </Gutter>
     </DefaultTemplate>
-  );
-};
+  )
+}
 
-export default AnalyticsDefaultRootView;
+export default AnalyticsDefaultRootView
--- src/graphics/Icon.tsx
+++ src/graphics/Icon.tsx
@@ -1,17 +1,6 @@
-'use client'
-
 import { HomeIcon } from 'lucide-react'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import React from 'react'
 
 export default function Icon() {
-  const pathname = usePathname()
-  const icon = <HomeIcon size={17} className="text-primary" />
-
-  if (pathname === '/admin/analytics') {
-    return <Link href="/admin">{icon}</Link>
-  }
-
-  return icon
+  return <HomeIcon size={17} className="text-primary" />
 }
